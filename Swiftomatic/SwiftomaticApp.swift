318ac20664d80fbab09f85fe1d6461c161d93995
Malovsky <kevinmaslowski@gmail.com>
2021-03-16 14:40:46 +0100

    Initial Commit

---
+++ Swiftomatic/SwiftomaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftomaticApp.swift
+//  Swiftomatic
+//
+//  Created by Kevin MASLOWSKI on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftomaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
